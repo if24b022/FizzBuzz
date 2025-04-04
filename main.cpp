db6fad9adc20830a4af437b6e7ea1c4a2b2614b6
Daniel Wucher <if24b022@technikum-wien.at>
2025-04-04 10:25:21 +0200

    6

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
     for(int i = 0; i <= 150; i++){
         if(i % 15 == 0){
             cout << "FizzBuzz\n";
-        }else {
+        }else if(i % 3 == 0){
             cout << "Fizz\n";
         }
     }
