19cd1415c7ac03110c0182b2d4edbb77ec6a98a3
Daniel Wucher <if24b022@technikum-wien.at>
2025-04-04 11:01:54 +0200

    27

--- main.cpp
+++ main.cpp
@@ -6,12 +6,15 @@ string fizzbuzz(int zahl){
     if(zahl % 3 == 0 && zahl % 5 == 0){
         return "FizzBuzz";
     }
+    if(zahl % 3 == 0){
+        return "Fizz";
+    }
 }
 
 int main()
 {
     for(int i = 0; i <= 150; i++){
-        fizzbuzz(i);
+        cout << fizzbuzz(i) << "\n";
     }
     return 0;
 }
